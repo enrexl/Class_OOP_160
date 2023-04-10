e20e3036753997f1dd8fbd8295ae1aac5f764151
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-04-10 14:46:00 +0700

    Membuat Class Matakuliah

--- OOP_ClassObject/OOP_ClassObject.cpp
+++ OOP_ClassObject/OOP_ClassObject.cpp
@@ -10,6 +10,22 @@ public:
         cout << "\nNama = " << nama;
     }
 };
+
+class MataKuliah() {
+private:
+    int sks;
+    string kode;
+    string namaMk;
+public:
+    void input() {
+        cout << "\nMasukkan Kode Matakuliah = ";
+        cin >> kode;
+        cout << "Masukkan Matakuliah = ";
+        cin >> namaMk;
+        cout << "Jumlah SKS = ";
+        cin >> sks;
+    }
+}
 int main()
 {
     std::cout << "Hello World!\n";
